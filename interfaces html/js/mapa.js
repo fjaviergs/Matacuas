fcc94f0c5a10aea47ce6059c0a91c7f5ede5150f
wsmanusan <manu92@gmail.com>
2016-03-12 20:19:00 +0100

    Cosas añadidas:   - Se selecciona de un mapa el lugar del suceso   - Deja seleccionar foto al crear reporte   - El anterior main.html ahora es index.html   - En main.html se tienen reportes falsos para ver como queda   - Al entrar en un reporte se ve en detalle   - Hay un formulario para responder acusaciones

---
+++ interfaces html/js/mapa.js
@@ -0,0 +1,62 @@
+var mapa;
+var geocoder;
+
+$(document).ready(function() {
+	mapa = document.getElementById("mapa");
+	geocoder = new google.maps.Geocoder;
+	document.getElementById("state").value = "Seleccionar dirección en el mapa."
+
+	
+	if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+	} else {
+		alert("Problemas con la geolocalización");
+	}
+});
+
+function showPosition(position) {
+	var lat = position.coords.latitude;
+	var lon = position.coords.longitude;
+	crearMapa(lat, lon);
+}
+
+function crearMapa(lat, lon) {
+	mapa.style.height = "400px";
+	mapa.style.width = document.getElementById("state").width;
+	
+	var coords = new google.maps.LatLng(lat, lon);
+	var options = {
+		zoom: 16,
+		center: coords,
+		mapTypeControl: false,
+		navigationControlOptions: {
+			style: google.maps.NavigationControlStyle.SMALL
+		},
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	var map = new google.maps.Map(mapa, options);
+	
+	var marker = new google.maps.Marker({
+		position: coords, 
+		map: map
+	});
+	
+	google.maps.event.addListener(map, "click", function(event) {
+		marker.setPosition(event.latLng);
+
+		geocoder.geocode({
+			'location': event.latLng
+		  }, function (results, status) {
+			if (status === google.maps.GeocoderStatus.OK) {
+			  if (results[1]) {
+				document.getElementById("state").value = results[0].formatted_address;
+			  } else {
+				alert('No results found');
+			  }
+			} else {
+			  alert('Geocoder failed due to: ' + status);
+			}
+	  });
+		
+	});
+}
